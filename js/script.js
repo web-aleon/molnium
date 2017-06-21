449e730e5cac34ddd56606c560e5abcdd2af2c44
Aleksey <web-a.leon@ukr.net>
2017-06-21 03:22:29 +0300

    adaptiv mobile before #results

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function wbh_mobilebtn(){
+	let menu = document.getElementById('headermenu');
+
+	if (menu.classList.contains('mobilemenushow')){
+		menu.classList.remove('mobilemenushow');
+	}else{
+		menu.classList.add('mobilemenushow');
+	}
+}
+
+let btn = document.getElementById('mobilemenu');
+btn.onclick = wbh_mobilebtn;
